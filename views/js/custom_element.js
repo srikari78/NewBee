3720e7af4b02c186973debda38499f6b0197c424
srikari <101363510+srikari78@users.noreply.github.com>
2024-03-05 15:07:06 -0800

    Add files via upload

---
+++ views/js/custom_element.js
@@ -0,0 +1,78 @@
+class MyFooter extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <!-- FOOTER -->
+        <footer id="footer">
+             <div class="container">
+                  <div class="row">
+   
+                       <div class="col-md-4 col-sm-6">
+                            <div class="footer-info">
+                                 <div class="section-title">
+                                      <h2>Headquarter</h2>
+                                 </div>
+                                 <address>
+                                      <p>San Jose <br>95113</p>
+                                 </address>
+   
+                                 <ul class="social-icon">
+                                      <li><a href="#" class="fa fa-facebook-square" attr="facebook icon"></a></li>
+                                      <li><a href="#" class="fa fa-twitter"></a></li>
+                                      <li><a href="#" class="fa fa-instagram"></a></li>
+                                 </ul>
+   
+                                 
+                            </div>
+                            
+                       </div>
+   
+                       <div class="col-md-4 col-sm-6">
+                            <div class="footer-info">
+                                 <div class="section-title">
+                                      <h2>Contact Info</h2>
+                                 </div>
+                                 <address>
+                                      <p>+1(998)888-4885</p>
+                                      <p><a href="mailto:contact@company.com">ssa@newbee.com</a></p>
+                                 </address>
+   
+                                 <div class="footer_menu">
+                                      <h2>Quick Links</h2>
+                                      <ul>
+                                           <li><a href="Main.html">Home</a></li>
+                                           <li><a href="about-us.html">About Us</a></li>
+                                           <li><a href="terms.html">Terms & Conditions</a></li>
+                                           <li><a href="contact.html">Contact Us</a></li>
+                                      </ul>
+                                 </div>
+                            </div>
+                       </div>
+   
+                       <div class="col-md-4 col-sm-12">
+                            <div class="footer-info newsletter-form">
+                                 <div class="section-title">
+                                      <h2>Newsletter Signup</h2>
+                                 </div>
+                                 <div>
+                                      <div class="form-group">
+                                           <form action="#" method="get">
+                                                <input type="email" class="form-control" placeholder="Enter your email" name="email" id="email" required>
+                                                <input type="submit" class="form-control" name="submit" id="form-submit" value="Send me">
+                                           </form>
+                                           <span><sup>*</sup> Please note - we do not spam your email.</span>
+                                      </div>
+                                 </div>
+                            </div>
+                       </div>
+                       
+                  </div>
+             </div>
+        </footer>
+        `
+    }
+}
+
+
+
+customElements.define('custom-footer', MyFooter)
+
